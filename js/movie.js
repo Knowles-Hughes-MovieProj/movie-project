1f7d1ab0763e81673bd0dd1cd8e443e78439cf49
Zackary Hughes <zackary.k.hughes@gmail.com>
2023-06-09 16:27:39 -0500

    Finished function for each card for Users to edit information.

--- js/movie.js
+++ js/movie.js
@@ -4,46 +4,47 @@
         .then(data => console.log(data))
         .catch(error => console.error(error));
 
+    let userEditTitle = document.querySelector('#editTitle')
+    let userEditRating = document.querySelector('#editRating')
+
     // $.ajax({
     fetch("https://tartan-leaf-yumberry.glitch.me/movies")
         .then(resp => resp.json()).then(data => {
         console.log(data);
         document.getElementsByClassName('row')[0].innerHTML = renderMovies(data)
         attachListeners(data);
-
-        $('#editBtn-1').on('click', function (data) {
-            console.log(data)
-            console.log(document.getElementById("movie-title-1").innerText);
-            const url = `https://tartan-leaf-yumberry.glitch.me/movies/${data.id}`;
-            const options = {
-                method: "PATCH",
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({title: `${userEditTitle}`, rating: `${userEditRating}`}),
-            };
-            // fetch(url, options)
-            //     .then(response => response.json())
-            //     .then(data => document.getElementsByClassName('row')[0].innerHTML += renderMovies(data))
-            //     .catch(/* handle errors */);
-        })
     });
 
     function attachListeners(data){
-
-        data.forEach(function(){
-            $(`#editBtn-${data.id}`).on("click", function(e){
-
+        console.log(data)
+        data.forEach(function(movie){
+            $(`#editBtn-${movie.id}`).on("click", function(e){
+                e.preventDefault()
+                console.log(document.getElementById(`movie-title-${movie.id}`).innerText);
+                console.log(document.getElementById(`movie-rating-${movie.id}`).innerText);
+                const url = `https://tartan-leaf-yumberry.glitch.me/movies/${movie.id}`;
+                const options = {
+                    method: "PATCH",
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({title: `${userEditTitle.value}`, rating: `${userEditRating.value}`}),
+                };
+                fetch(url, options)
+                    .then(response => response.json())
+                    .then(data => {
+                        console.log(data)
+                        $(`#${data.id}`).replaceWith(updateMovies(data))
+                        // document.getElementsByClassName('row')[0].innerHTML += updateMovies(data)
+                    })
+                    .catch(/* handle errors */);
             })
-
         })
-
     }
 
     function moviesRender(data) {
         let html = "";
-        html = '<div class="container d-flex justify-content-center col-12"></div>'
-        html = '<div class="card text-center m-2 p-3 col-12 col-md-2">';
+        html = `<div id="${data.id}" class="card text-center m-2 p-3 col-12 col-md-2">`;
         html += `<div class=" title m-0" id="movie-title-${data.id}"><h2> ${data.title} <h2/></div>`;
         html += `<span class="fs-6" id="movie-rating-${data.id}"> this is the rated ${data.rating} out of 5 stars </span>
         <a href="#" id="editBtn-${data.id}" class="btn m-2 btn-success">Edit</a><a href="#" id="deleteBtn-${data.id}" class="btn m-2 btn-danger">Delete</a></div>`;
@@ -60,8 +61,8 @@
         return html;
     }
 
-    function updateMovies(){
-
+    function updateMovies(data){
+        return moviesRender(data);
     }
 
 
@@ -83,25 +84,5 @@
             .then(data => document.getElementsByClassName('row')[0].innerHTML += moviesRender(data))
             .catch(/* handle errors */);
     })
-
-    let userEditTitle = document.querySelector('#editTitle')
-    let userEditRating = document.querySelector('#editRating')
-
-    // $('#editBtn-1').on('click', function (data) {
-    //        console.log(data)
-    //     console.log(document.getElementById("movie-title-1").value);
-    //     const url = `https://tartan-leaf-yumberry.glitch.me/movies/${data.id}`;
-    //     const options = {
-    //         method: "PATCH",
-    //         headers: {
-    //             'Content-Type': 'application/json',
-    //         },
-    //         body: JSON.stringify({title: `${userEditTitle}`, rating: `${userEditRating}`}),
-    //     };
-    //     fetch(url, options)
-    //         .then(response => response.json())
-    //         .then(data => document.getElementsByClassName('row')[0].innerHTML += renderMovies(data))
-    //         .catch(/* handle errors */);
-    // })
 
 })();
