dc53a73813bfdd74091b93ea46ba2db2e43f78a0
Kof'i Knowles <JemalKnowles1@gmail.com>
2023-06-09 14:47:45 -0500

    update

--- js/movie.js
+++ js/movie.js
@@ -9,15 +9,44 @@
         .then(resp => resp.json()).then(data => {
         console.log(data);
         document.getElementsByClassName('row')[0].innerHTML = renderMovies(data)
+        attachListeners(data);
+
+        $('#editBtn-1').on('click', function (data) {
+            console.log(data)
+            console.log(document.getElementById("movie-title-1").innerText);
+            const url = `https://tartan-leaf-yumberry.glitch.me/movies/${data.id}`;
+            const options = {
+                method: "PATCH",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({title: `${userEditTitle}`, rating: `${userEditRating}`}),
+            };
+            // fetch(url, options)
+            //     .then(response => response.json())
+            //     .then(data => document.getElementsByClassName('row')[0].innerHTML += renderMovies(data))
+            //     .catch(/* handle errors */);
+        })
     });
+
+    function attachListeners(data){
+
+        data.forEach(function(){
+            $(`#editBtn-${data.id}`).on("click", function(e){
+
+            })
+
+        })
+
+    }
 
     function moviesRender(data) {
         let html = "";
         html = '<div class="container d-flex justify-content-center col-12"></div>'
         html = '<div class="card text-center m-2 p-3 col-12 col-md-2">';
-        html += `<div class=" title m-0" data-${data.title}><h2> ${data.title} <h2/></div>`;
-        html += `<span class="fs-6"> this is the rated ${data.rating} out of 5 stars </span>
-        <a href="#" id= "editBtn-${data.id}" class="btn m-2 btn-success">Edit</a><a href="#" id="deleteBtn-${data.id}" class="btn m-2 btn-danger">Delete</a></div>`;
+        html += `<div class=" title m-0" id="movie-title-${data.id}"><h2> ${data.title} <h2/></div>`;
+        html += `<span class="fs-6" id="movie-rating-${data.id}"> this is the rated ${data.rating} out of 5 stars </span>
+        <a href="#" id="editBtn-${data.id}" class="btn m-2 btn-success">Edit</a><a href="#" id="deleteBtn-${data.id}" class="btn m-2 btn-danger">Delete</a></div>`;
         html += '</div>';
 
         return html;
@@ -29,6 +58,10 @@
             html += moviesRender(dataSet[i]);
         }
         return html;
+    }
+
+    function updateMovies(){
+
     }
 
 
@@ -54,25 +87,21 @@
     let userEditTitle = document.querySelector('#editTitle')
     let userEditRating = document.querySelector('#editRating')
 
-    $('#editBtn-1').on('click', function (data) {
-           console.log(data)
-        document.querySelector()
-        const url = `https://tartan-leaf-yumberry.glitch.me/movies/${data.id}`;
-        const options = {
-            method: "PATCH",
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({title: `${userEditTitle}`, rating: `${userEditRating}`}),
-        };
-        fetch(url, options)
-            .then(response => response.json())
-            .then(data => document.getElementsByClassName('row')[0].innerHTML += renderMovies(data))
-            .catch(/* handle errors */);
-    })
+    // $('#editBtn-1').on('click', function (data) {
+    //        console.log(data)
+    //     console.log(document.getElementById("movie-title-1").value);
+    //     const url = `https://tartan-leaf-yumberry.glitch.me/movies/${data.id}`;
+    //     const options = {
+    //         method: "PATCH",
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //         },
+    //         body: JSON.stringify({title: `${userEditTitle}`, rating: `${userEditRating}`}),
+    //     };
+    //     fetch(url, options)
+    //         .then(response => response.json())
+    //         .then(data => document.getElementsByClassName('row')[0].innerHTML += renderMovies(data))
+    //         .catch(/* handle errors */);
+    // })
 
 })();
-
-
-
-
